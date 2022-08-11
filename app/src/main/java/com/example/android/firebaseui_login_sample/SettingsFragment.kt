e713cd3a3965f7170b5c1af5519f0419a67c830a
Katarzyna Bedkowska <kbedkowska@C12339>
2022-08-11 09:12:21 +0200

    first commit

--- app/src/main/java/com/example/android/firebaseui_login_sample/SettingsFragment.kt
+++ app/src/main/java/com/example/android/firebaseui_login_sample/SettingsFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import androidx.preference.PreferenceFragmentCompat
 
